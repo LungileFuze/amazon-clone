f3856642c183955bd474ad9c8148a2e2dabcd837
LungileFuze <ngcobolungile94@outlook.com>
2023-10-22 11:15:57 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Route} from 'react-router-dom';
+import Home from './components/Home';
+import Products from './components/Products';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Amazon Clone</h1>
+      <Route path='/home'>
+        <Home />
+      </Route>
+      <Route path='/products'>
+        <Products />
+      </Route>
+      
     </div>
   );
 }
